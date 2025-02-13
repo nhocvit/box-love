4c07815e158cd15819fcaa382cc010a7652d2064
nhocvit <trngkhnh04@gmail.com>
2025-02-13 16:17:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
     titleWeb: "For Minh",
 
-    background: "z6312296276902_3398bdc7281ced4e33aa71b328d9dd55.jpg",
+    background: "images/z6312294924505_7918990cfd1df7ea3a43061b0e9bb838.jpg",
 
-    min1: "min1.jfif",
-    min2: "min2.jpg",
-    min3: "max3.jpg",
-    min4: "max4.webp",
-    min5: "min5.jpg",
-    min6: "min6.jpg",
+    min1: "images/z6312294925197_8a864987b0d18eb1af62ac652c2ef420.jpg",
+    min2: "images/z6312296276902_3398bdc7281ced4e33aa71b328d9dd55.jpg",
+    min3: "images/z6312296277121_7f4a38cad09d5bc28a9834448c26eb6c.jpg",
+    min4: "images/z6312296279149_4d11cbf3ee8916bc42a2f580b7118423.jpg",
+    min5: "images/z6312296279231_e8915e29987dc65b66e99bd5b890720d.jpg",
+    min6: "images/z6312296280753_9ca2a57d5ce9585bbafb9b1aeea5eb3f.jpg",
 
-    max1: "max1.jpg",
-    max2: "max2.jpg",
-    max3: "max3.jpg",
-    max4: "max4.webp",
-    max5: "max5.png",
-    max6: "max6.jpg", 
+    max1: "images/z6312296282487_45313cc3f271d6b40fdbc1e37fab883c.jpg",
+    max2: "images/z6312296284389_e6ecea0a349c17c85416d9485a8ba15d.jpg",
+    max3: "images/z6312296287153_0d25717ee2d580f3837695d338904468.jpg",
+    max4: "images/z6312296894051_b8614a6ac1b1f285cbb76908157bfbba.jpg",
+    max5: "images/z6312296896223_29fd7c45a60d1d5dc6565086180210c7.jpg",
+    max6: "images/z6312296896377_9639a92c6f221a65735c0396cd7e874f.jpg", 
 }
